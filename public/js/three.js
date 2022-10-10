c16fe7deb87fd3ba18378b684375270cd28c5373
r0808 <arthurdelophem3@gmail.com>
2022-10-10 20:02:42 +0200

    feat: add orbit controls for camera

--- public/js/three.js
+++ public/js/three.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -9,22 +10,28 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const light = new THREE.SpotLight()
-light.position.set(5, 5, 5)
-scene.add(light)
+const controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0, 0.2, 0.4);
+controls.update();
+
+const light = new THREE.SpotLight();
+light.position.set(5, 5, 5);
+scene.add(light);
 
 const loader = new GLTFLoader();
 loader.load('../assets/donunq_object.glb', (gltf) => {
-    scene.add(gltf.scene)
+    scene.add(gltf.scene);
 })
 
 function animate() {
-    requestAnimationFrame(animate)
-    render()
+    requestAnimationFrame(animate);
+    controls.update();
+    render();
 }
 
 function render() {
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 }
 
 animate()
