7e17de8dc85939cd662b5c13f2cdaa54d57e96f8
r0808 <arthurdelophem3@gmail.com>
2022-10-10 19:50:42 +0200

    feat: add light + 3d rendering

--- public/js/three.js
+++ public/js/three.js
@@ -1,8 +1,25 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 0.4;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const light = new THREE.SpotLight()
+light.position.set(5, 5, 5)
+scene.add(light)
+
+function animate() {
+    requestAnimationFrame(animate)
+    render()
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
