e8f1e558f281c79f54e6a997dab3334d46f79984
r0808 <arthurdelophem3@gmail.com>
2022-10-11 23:16:32 +0200

    feat: add venus planet to three viewport

--- public/js/three.js
+++ public/js/three.js
@@ -5,7 +5,6 @@ import { HDRCubeTextureLoader } from "three/addons/loaders/HDRCubeTextureLoader.
 import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 import { sRGBEncoding } from 'three';
 
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 0.4;
@@ -20,6 +19,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 camera.position.set(0, 0.2, 0.4);
 controls.update();
 
+//hdri
 const rgbeLoader = new RGBELoader();
 rgbeLoader.load("../assets/quattro_canti_4k.hdr", (texture) => {
     texture.mapping = THREE.EquirectangularReflectionMapping;
@@ -27,14 +27,24 @@ rgbeLoader.load("../assets/quattro_canti_4k.hdr", (texture) => {
     scene.environment = texture;
 })
 
+//lights
 const light = new THREE.SpotLight();
 light.position.set(5, 5, 5);
 scene.add(light);
 
+//donunq
 const loader = new GLTFLoader();
 loader.load("../assets/donunq_object.glb", (gltf) => {
     scene.add(gltf.scene);
 })
+
+//Planet
+const planetTexture = new THREE.TextureLoader().load("../assets/2k_venus_surface.jpg");
+const planetGeo = new THREE.SphereGeometry(0.05)
+const planetMaterial = new THREE.MeshBasicMaterial({ map: planetTexture });
+const planet = new THREE.Mesh(planetGeo, planetMaterial);
+planet.position.set(0.3, 0.3, 0.3)
+scene.add(planet);
 
 function animate() {
     requestAnimationFrame(animate);
